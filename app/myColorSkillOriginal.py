771077830183ec1fd32632ca5023ab435bdf15d5
Yasmeen Hazy <yasmeen.hazy@gmail.com>
2017-09-07 12:51:57 +0100

    Alexa asks for child's name and gives correct response. Needs a bit more work.

--- app/myColorSkillOriginal.py
+++ app/myColorSkillOriginal.py
@@ -52,13 +52,11 @@ def get_welcome_response():
     card_title = "Welcome"
     speech_output = "Welcome to your child companion setup. "\
                     "Please tell me the name of the child that will be "\
-                    "using this application by saying, " \
-                    "my favorite color is red"
+                    "using this application. "
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
     reprompt_text = "Please tell me the name of the child that will be "\
-                    "using this application by saying, " \
-                    "my favorite color is red"
+                    "using this application. "
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
@@ -74,12 +72,12 @@ def handle_session_end_request():
         card_title, speech_output, None, should_end_session))
 
 
-def create_child_name(child_name):
-    return {"childName": child_name}
+def create_favorite_color_attributes(favorite_color):
+    return {"favoriteColor": favorite_color}
 
 
-def set_child_name_in_session(intent, session):
-    """ Sets the child's name in the session and prepares the speech to reply to the
+def set_color_in_session(intent, session):
+    """ Sets the color in the session and prepares the speech to reply to the
     user.
     """
 
@@ -87,22 +85,21 @@ def set_child_name_in_session(intent, session):
     session_attributes = {}
     should_end_session = False
 
-    if 'Child' in intent['slots']:
-        child_name = intent['slots']['Child']['value']
-        session_attributes = create_child_name(child_name)
-        speech_output = "I now know your child's name is " + \
-                        child_name + \
-                        ". You can ask me your child's name by saying, " \
-                        "what's my child's name?"
-        reprompt_text = ". You can ask me your child's name by saying, " \
-                        "what's my child's name?"
+    if 'Color' in intent['slots']:
+        favorite_color = intent['slots']['Color']['value']
+        session_attributes = create_favorite_color_attributes(favorite_color)
+        speech_output = "I now know your favorite color is " + \
+                        favorite_color + \
+                        ". You can ask me your favorite color by saying, " \
+                        "what's my favorite color?"
+        reprompt_text = "You can ask me your favorite color by saying, " \
+                        "what's my favorite color?"
     else:
-        speech_output = "I'm not sure what your child's name is. " \
+        speech_output = "I'm not sure what your favorite color is. " \
                         "Please try again."
-        reprompt_text = "I'm not sure what your child's name is. " \
-                        "Please tell me the name of the child that will be "\
-                        "using this application by saying, " \
-                        "my favorite color is red"
+        reprompt_text = "I'm not sure what your favorite color is. " \
+                        "You can tell me your favorite color by saying, " \
+                        "my favorite color is red."
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
@@ -111,16 +108,14 @@ def get_color_from_session(intent, session):
     session_attributes = {}
     reprompt_text = None
 
-    if session.get('attributes', {}) and "childName" in session.get('attributes', {}):
-        child_name = session['attributes']['childName']
-        speech_output = "Your child's name is " + child_name + \
+    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
+        favorite_color = session['attributes']['favoriteColor']
+        speech_output = "Your favorite color is " + favorite_color + \
                         ". Goodbye."
         should_end_session = True
     else:
-        speech_output = "I'm not sure what your child's name is. " \
-                        "Please tell me the name of the child that will be "\
-                        "using this application by saying, " \
-                        "my favorite color is red"
+        speech_output = "I'm not sure what your favorite color is. " \
+                        "You can say, my favorite color is red."
         should_end_session = False
 
     # Setting reprompt_text to None signifies that we do not want to reprompt
@@ -160,8 +155,8 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "MyChildsNameIs":
-        return set_child_name_in_session(intent, session)
+    if intent_name == "MyColorIsIntent":
+        return set_color_in_session(intent, session)
     elif intent_name == "WhatsMyColorIntent":
         return get_color_from_session(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
